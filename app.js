0c069b62ce13c055a64c87d033771459f45182da
milad <melad.km26@gmail.com>
2024-12-14 22:26:02 +0300

    fic css

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const apiEndpoint = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 
 async function fetchData() {
+
+    $response = file_get_contents($URL);
+    $result = json_decode($response, true);
   try {
     const response = await fetch(apiEndpoint);
     if (!response.ok) throw new Error("Failed to fetch data");
